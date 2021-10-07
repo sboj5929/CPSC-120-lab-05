128bc935c34de8b24c2b53be147d8e6eced6afb1
/home/sboj5929/.gitconfig <sboj5929@csu.fullerton.edu>
2021-10-07 13:53:05 -0700

    I added the gregrial formula so then we can convert from Gregorial to Julian.

--- part-1/datediff.cc
+++ part-1/datediff.cc
@@ -21,7 +21,10 @@ using namespace std;
 int GregorianToJulian(int day, int month, int year) {
   int julian_day_number = 0;
 
-  // TODO: write the implementation
+  julian_day_number = day - 32075 + 1461
+      * (year + 4800 + (month - 14) / 12) / 4
+      + 367 * (month - 2 - (month - 14) / 12 * 12) / 12 - 3
+      * ((year + 4900 + (month - 14) / 12) / 100) / 4;
 
   return julian_day_number;
 }
